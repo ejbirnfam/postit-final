27245b847573b310859a23448f8b458b2f94b1ab
ejbirnfam <ericjbirnbaum@gmail.com>
2015-04-12 13:01:14 -0400

    added authetntication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :require_user
   def create
     @post = Post.find(params[:post_id])
     #@comment = Comment.new(params.require(:comment).permit(:body))
     #@comment.post = @post
     #these two lines can be combined to...
     @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment.creator = User.first #need to fix after authentication
+    @comment.creator = current_user
 
     if @comment.save
       flash[:notice] = 'Your comment was added'
